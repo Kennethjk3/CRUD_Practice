c1b0b2bb87f1532f208b77b95f4c4fe4280f1e51
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-07-12 01:16:29 -0400

    Links to root, Create

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,13 +12,28 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    # Saves to DB
+    @bookmark = Bookmark.create(bookmark_parms)
 
+    # # This way allows for testing
+    # @bookmark = Bookmark.new(bookmark_parms)
+    # if @bookmark.save
+    #   puts "Saved"
+    # else
+    #   puts "Try again"
+    # end
+    redirect_to action: :index
   end
 
 
 
 
 
+  private
+
+  def bookmark_parms
+    params.require(:bookmark).permit(:url,  :title, :comment, :favorite)
+  end
 
 
 
